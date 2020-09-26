4232c0921e089fe3dd73ffe5f1c38344b27f24ce
Abhay Kumar Dhar <abhaykumardhar@gmail.com>
2020-09-25 20:43:51 -0700

    Add files via upload

---
+++ Ass2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Ass2
+    {
+        public static bool isprimeNumber(int num)
+        {
+            for (int i = 2; i < num; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+        public static void Main()
+        {
+
+            Console.Write("Enter a number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (isprimeNumber(n))
+                Console.WriteLine(n + " is a prime number");
+            else
+                Console.WriteLine(n + " is not a prime number");
+        }
+    }
+}
---
+++ Ass5.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Book
+    {
+
+        int id;
+        string title;
+        float price;
+        string author;
+        public int Bookid
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Booktitle
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public float Bookprice
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public string Bookauthor
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        class Ass5
+        {
+            static void Main(string[] args)
+            {
+                Book b = new Book();
+                b.id = 123;
+                b.title = "Game of theones";
+                b.price = 100;
+                b.author = "George RR Martin";
+                Console.Write("The book id ,title ,price and autor is {0} {1} {2} {3} ", b.id, b.title, b.price, b.author);
+
+            }
+
+        }
+    }
+}
